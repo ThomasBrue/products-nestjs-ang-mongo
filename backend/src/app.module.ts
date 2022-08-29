499add7cb04fc59b0ae33c2164cc3b3140f7cbbb
ThomasBrue <t.bruendl@gmail.com>
2022-08-29 23:15:24 +0200

    run frontend in prod mode

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,15 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '.', 'frontend'),
+    }),
     ProductsModule,
     MongooseModule.forRoot(
       // 'mongodb+srv://kaiserkarl:7Katzenaugen@cluster0.29by76y.mongodb.net/nestjs-demo?retryWrites=true&w=majority',
       'mongodb+srv://kaiserkarl:U1Sb2xeUuO7WtySD@cluster0.29by76y.mongodb.net/?retryWrites=true&w=majority',
     ),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [], // [AppController],
+  providers: [], //[AppService],
 })
 export class AppModule {}
